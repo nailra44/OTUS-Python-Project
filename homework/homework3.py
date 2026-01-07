1736b59cdb35a7e72151a802386a150f00388848
nailra44 <Merge branch 'main' of https://github.com/nailra44/OTUS-Python-Project # Please enter a commit message to explain why this merge is necessary,                                                                                                                                                              # especially if it merges an updated upstream into a topic branch.                                                                                                                                                                   # # Lines starting with '#' will be ignored, and an empty message aborts # the commit. ~                                                                                                                                                                                                                                   ~                                                                                                                                                                                                                                   ~                                                                                                                                                                                                                                   ~                                                                                                                                                                                                                                   ~                                                                                                                                                                                                                                   ~                                                                                                                                                                                                                                   ~                  nailra44@yandex.ru>
2026-01-07 12:29:01 +0300

    add metod numeric even, odd

--- homework/homework3.py
+++ homework/homework3.py
@@ -16,8 +16,29 @@ def power2(*args, p=2):
     return lol, orig
 
 
+def numerik(*args):
+    for num in args:
+        if num % 2 == 0:
+            print(f"{num} : четное число")
+        else:
+            print(f"{num} : нечетное число")
+
+def numerik2(*args, filters='even'):
+    if filters == 'even':
+     for num in args:
+         if num % 2 == 0:
+             print(f"{num} : четное")
+    elif filters == 'odd':
+        for num in args:
+            if num % 2 != 0:
+                print(f"{num} : нечетое")
 
 
+
+
+numerik2(2,8,6,5,4,7,8,9,filters='even')
+
+#numerik(2,8,6,5,4,7,8,9,)
 num = power2(1,2,3,4, p=6)
 print(num)
 #num2 = power(5, 15)
